258b127e31b5c50f7ad885330bfb60c556bd9f79
Makijane Gilbert <makijaneg@gmail.com>
2019-08-16 16:10:08 -0700

    added functions to display results and append info into result, append name and show result info.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("form#program").submit(function(event){
-    var name = $("input#username").val();
+    var name = $("input#user_name").val();
 
     var experience = parseInt($("input:radio[name=experience]:checked").val());
     var interests = $("select#interests").val();
@@ -10,6 +10,7 @@ $(document).ready(function(){
     var meals = $("select#meals").val();
 
     var resultOptions = ["Python", "Swift", "JavaScript", "C#"]
+    var resultInfoOptions = ["https://www.python.org/", "https://developer.apple.com/swift/", "https://www.javascript.com/", "https://docs.microsoft.com/en-us/dotnet/csharp/"]
     var result;
 
     if (experience === 0 && abilities <= 2) {
@@ -19,11 +20,24 @@ $(document).ready(function(){
     } else if (experience === 2 && abilities >= 3){
       result = resultOptions[2];
     }else {
-      result = 
-    }
-$("#name").empty().append(name);
-$("#result")
-$("#program-pair").show();
-    });
+      result = resultOptions[Math.floor(Math.random() * resultOptions.length)];
+    };
 
+    if (result === "Python"){
+      $(#result-info).empty.append(<a href=resultInfoOptions[0]>resultInfoOptions[0]</a>);
+    }else if (result === "Swift"){
+      $(#result-info).empty.append(resultInfoOptions[1]);
+    }else if (result === "JavaScript"){
+      $(#result-info).empty.append(resultInfoOptions[2]);
+    }else if (result === "C#"){
+      $(#result-info).empty.append(resultInfoOptions[3]);
+    }else {
+
+    }
+    $("#name").empty().append(name);
+    $("#result").empty().append(result);
+    $("#program-pair").show();
+
+    event.preventDefault();
+    });
   });
