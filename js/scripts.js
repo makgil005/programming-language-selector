b335193333ea9835811108b20f1f05df3fe4a954
Makijane Gilbert <makijaneg@gmail.com>
2019-08-16 16:29:27 -0700

    completed javascript interactions, starting styles.

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
     var meals = $("select#meals").val();
 
     var resultOptions = ["Python", "Swift", "JavaScript", "C#"]
-    var resultInfoOptions = ["https://www.python.org/", "https://developer.apple.com/swift/", "https://www.javascript.com/", "https://docs.microsoft.com/en-us/dotnet/csharp/"]
     var result;
 
     if (experience === 0 && abilities <= 2) {
@@ -21,21 +20,10 @@ $(document).ready(function(){
       result = resultOptions[2];
     }else {
       result = resultOptions[Math.floor(Math.random() * resultOptions.length)];
-    };
-
-    if (result === "Python"){
-      $(#result-info).empty.append(<a href=resultInfoOptions[0]>resultInfoOptions[0]</a>);
-    }else if (result === "Swift"){
-      $(#result-info).empty.append(resultInfoOptions[1]);
-    }else if (result === "JavaScript"){
-      $(#result-info).empty.append(resultInfoOptions[2]);
-    }else if (result === "C#"){
-      $(#result-info).empty.append(resultInfoOptions[3]);
-    }else {
-
     }
     $("#name").empty().append(name);
     $("#result").empty().append(result);
+    $("#result-info").show();
     $("#program-pair").show();
 
     event.preventDefault();
