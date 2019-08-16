7c7110183bb00a09d11f74f558c13606a99982b9
Makijane Gilbert <makijaneg@gmail.com>
2019-08-16 15:10:57 -0700

    added variables for result and spans to insert results in text, if else statements and a list of programming languages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
   $("form#program").submit(function(event){
-    var name = $("input#name").val();
+    var name = $("input#username").val();
 
-    var experience = $("input:radio[name=experience]:checked").val();
+    var experience = parseInt($("input:radio[name=experience]:checked").val());
     var interests = $("select#interests").val();
     var color = $("#color").val();
     var transportation = $("input:radio[name=transportation]:checked").val();
     var abilities = $("select#abilities").val();
     var meals = $("select#meals").val();
 
+    var resultOptions = ["Python", "Swift", "JavaScript", "C#"]
+    var result;
+
+    if (experience === 0 && abilities <= 2) {
+      result = resultOptions[0];
+    } else if (experience === 1 && abilities <= 2){
+      result = resultOptions[1];
+    } else if (experience === 2 && abilities >= 3){
+      result = resultOptions[2];
+    }else {
+      result = 
+    }
+$("#name").empty().append(name);
+$("#result")
+$("#program-pair").show();
     });
 
   });
